6fc2bcac368dc8d1bd9593822d223c8e8469f5bf
Melik fırat Başçi <mbasci23@hotmail.com>
2021-03-26 20:02:10 +0300

    17.gun-3-apiConnection

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-  products:Product[]=[];  
-  constructor() { }
+  products: Product[] = [];
+  apiUrl = 'https://localhost:44369/api/products/getall';
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
+    //component calistiginda ilk calisan kod ctor degildir.
+    this.getProducts();
   }
 
+  getProducts() {
+    this.httpClient
+    .get<ProductResponseModel>(this.apiUrl)//gelen data productResponseModele Map edilecek.
+    .subscribe((response) => {//yanit basarili sekilde geldiginde napayim?
+      this.products=response.data;//response datasini product nesnesine ata  
+    }); 
+  }
 }
