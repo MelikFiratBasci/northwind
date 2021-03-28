0806a6352c153f7d63ac02d52fbf458837980b1a
Melik fırat Başçi <mbasci23@hotmail.com>
2021-03-28 17:45:32 +0300

    18.gun-1-genericResponse+category

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ProductResponseModel } from '../models/productResponseModel';
-
+import { ListResponseModel } from '../models/listResponseModel';
+import { Product } from '../models/product';
 @Injectable({
   providedIn: 'root',
 })
@@ -10,7 +10,7 @@ export class ProductService {
   apiUrl = 'https://localhost:44369/api/products/getall';
   constructor(private httpClient: HttpClient) {}
 
-  getProducts():Observable<ProductResponseModel> {//subscribe olunabilir bir responsemodel doneceksin 
-    return this.httpClient.get<ProductResponseModel>(this.apiUrl); //gelen data productResponseModele Map edilecek.
+  getProducts():Observable<ListResponseModel<Product>> {//subscribe olunabilir bir responsemodel doneceksin 
+    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl); //gelen data productResponseModele Map edilecek.
   }
 }
