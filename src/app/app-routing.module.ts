876794468edc14890abb10f7cc45a0720cebb019
Melik fırat Başçi <mbasci23@hotmail.com>
2021-03-28 18:20:15 +0300

    18.gun-2-component-Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",pathMatch:"full",component:ProductComponent},//anasayfa
+  {path:"products",component:ProductComponent},//product adresine giderse ne gostereyim
+  
+];//router-outlette ne gostereyim
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)],//uygulamamiz icin rootlari devreye al
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
